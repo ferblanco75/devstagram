3a9d551430f419e8f45ee7074f3b9f514aadba3d
ferblanco75 <ferblanco@gmail.com>
2025-03-06 19:55:11 -0300

    agrego vista y controlador de login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     public function index(){
-        
+        return view('auth.login');
+    }
+
+    public function store(){
+        dd('autenticando ....');
     }
 }
