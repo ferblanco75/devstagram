35a35468e71cfc1206339c272c95ffc5ce354e57
fernando <ferblanco@gmail.com>
2025-03-27 16:35:26 -0300

    modifico el app blade para que incluya dropzone

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Devstagram - @yield('titulo') </title>   
+        <title>Devstagram - @yield('titulo') </title>  
          @vite('resources/css/app.css')
+         @vite('resources/js/app.js') 
     </head>
 
     <body class="bg-gray-100">
