996d04e8423d946923bc03b7143466b68fc91f78
fernando <ferblanco@gmail.com>
2025-05-29 15:49:13 -0300

    agrego edicion de perfil para cambiar nombre

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+use App\Models\User;
+use App\Models\Post;
 
 class PerfilController extends \Illuminate\Routing\Controller
 {
@@ -17,5 +22,42 @@ class PerfilController extends \Illuminate\Routing\Controller
     
     public function index(){
         return view('perfil.index');
+    }
+
+    public function store(Request $request){
+
+        $request->request->add(['username' => Str::slug($request->username)]);
+
+        $validateData = $request->validate([
+            'username' => ['required','unique:users','min:3','max:20','not_in:twitter,editar-perfil'],
+        ]);
+
+        if($request->imagen) {
+           $imagen = $request->file('imagen');
+                // Genera un nombre único para la imagen
+            $nombreImagen = Str::uuid() . '.' . $imagen->getClientOriginalExtension();          
+        // Crear instancia del ImageManager
+            $manager = new ImageManager(new Driver()); 
+            $imagenServidor = $manager->read($imagen);
+
+            // Redimensionar la imagen a 1000x1000
+            $imagenServidor->cover(1000, 1000);
+            // Verificar si la carpeta 'uploads' existe, si no, la crea
+            $rutaUploads = public_path('uploads');
+            if (!file_exists($rutaUploads)) {
+                mkdir($rutaUploads, 0777, true);
+            }
+
+            // Guardar la imagen
+            $imagenPath = $rutaUploads . '/' . $nombreImagen;
+            $imagenServidor->save($imagenPath);
+        }
+        //guardar cambios
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request->username;
+        //$usuario->imagen = $nombreImagen ?? '';
+        $usuario->save();
+        //redirect al muro
+        return redirect()->route('posts.index', $usuario->username);
     }
 }
